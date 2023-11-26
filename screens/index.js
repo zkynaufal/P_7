7ede8ebc5e1853fded9f4ba70ec766604e0c4a56
Zaky Naufal A <93167071+zkynaufal@users.noreply.github.com>
2023-11-26 20:30:22 +0700

    Add files via upload

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import AboutScreen from "./about";
+import TaskScreen from "./task-all";
+import TaskCompletedScreen from "./task-completed";
+
+export { AboutScreen, TaskCompletedScreen, TaskScreen };
